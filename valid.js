9a1f8776f9d27cd831392591854b1eb3027ce9f7
Korang Pearl Antwi <pearlkorang1@gmail.com>
2023-09-18 12:53:37 +0000

    changed fisrtname to firstname

--- valid.js
+++ valid.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form');
-const firstname = document.getElementById('fisrtname');
+const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const telno = document.getElementById('telno');
 const email = document.getElementById('email');
