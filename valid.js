80020ff92b9531516ec3b9225a5eed60d0d8f597
Pearl28-me <115065232+Pearl28-me@users.noreply.github.com>
2023-09-21 16:28:38 +0000

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,12 +1,16 @@
+
 const form = document.getElementById('form');
-const firstname = document.getElementById('firstname');
-const lastname = document.getElementById('lastname');
-const telno = document.getElementById('telno');
+const firstname = document.getElementById('firstName');
+const lastname = document.getElementById('lastName');
+const telno = document.getElementById('telephone');
 const email = document.getElementById('email');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    validateInputs();
+
+    let query_string = new URLSearchParams(new FormData(e.target))
+   if(validateInputs())  window.location.href =`anotherpage.html?${query_string}`
+    
 });
 
 const setError = (element, message) => {
@@ -31,33 +35,54 @@ const validateInputs = () => {
     const telnoValue = telno.value.trim();
     const emailValue = email.value.trim();
 
-    if (firstnameValue === '') {
+    let res = true;
+
+    if (!firstnameValue) {
         setError(firstname, 'First name is required');
+        res = false
     } else {
         setSuccess(firstname);
     }
 
-    if (lastnameValue === '') {
+    if (!lastnameValue) {
         setError(lastname, 'Last name is required');
+        res = false
     } else {
         setSuccess(lastname);
     }
 
-    if (telnoValue === '') {
+    if (!telnoValue) {
         setError(telno, 'Telephone number is required');
+        res = false
     } else if (telnoValue.length === 10) {
         setSuccess(telno);
     } else {
         setError(telno, 'Telephone number should be 10 characters');
+        res = false
     }
 
-if (emailValue === '') {
+if (!emailValue) {
         setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue.includes('@'))) {
+        res = false
+    }
+     else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        res = false
     } else {
         setSuccess(email);
     }
-    
+
+
+            return res ;
 };
 
+
+
+
+
+const isValidEmail = (email) => {
+
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+}
